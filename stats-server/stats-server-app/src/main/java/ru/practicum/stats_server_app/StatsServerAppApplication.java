65914fbda86139b6699f428e7042a1b4dbfb6fe1
Umbasa <butuzov_tema@yahoo.com>
2025-09-09 14:46:46 +0300

    прикрутил модули

---
+++ stats-server/stats-server-app/src/main/java/ru/practicum/stats_server_app/StatsServerAppApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.stats_server_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatsServerAppApplication.class, args);
+    }
+
+}
